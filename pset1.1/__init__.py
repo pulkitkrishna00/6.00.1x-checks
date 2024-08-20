6099dc1960fb3ff5e1cad492d775d084941e4771
Pulkit Krishna <contact@virtualhub.eu.org>
2024-08-20 20:42:56 +0530

    pset 1.1 test 2

--- pset1.1/__init__.py
+++ pset1.1/__init__.py
@@ -1,18 +1,32 @@
 import check50
 
+
 @check50.check()
 def exists():
     """pset1.1.py exists"""
     check50.exists("pset1.1.py")
 
+
 @check50.check(exists)
 def azcbobobegghakl():
     """correctly identifies the number of vowels in azcbobobegghakl"""
-    file = open('pset1.1.py')
+    file = open("pset1.1.py")
     text = file.read()
     file.close()
-    pset = open('pset.py', 'w')
+    pset = open("pset.py", "w")
     pset.write("s = 'azcbobobegghakl'\n")
     pset.write(text)
     pset.close()
     check50.run("python3 pset.py").stdout("Number of vowels: 5").exit(0)
+
+
+def bzcbcbcbdgghfkl():
+    """correctly identifies the number of vowels in bzcbcbcbdgghfkl"""
+    file = open("pset1.1.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'bzcbcbcbdgghfkl'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of vowels: 0").exit(0)
