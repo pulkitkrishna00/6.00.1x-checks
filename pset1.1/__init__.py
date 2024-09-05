e6693adc597dbce29c4c4ce5a2e766e94bc9ef7a
Pulkit Krishna <contact@virtualhub.eu.org>
2024-09-05 13:06:26 +0530

    PSET 1.1

--- pset1.1/__init__.py
+++ pset1.1/__init__.py
@@ -8,26 +8,242 @@ def exists():
 
 
 @check50.check(exists)
-def azcbobobegghakl():
-    """correctly identifies the number of vowels in azcbobobegghakl"""
+def ilatudolzeiaufwuf():
+    """correctly identifies the number of vowels in ilatudolzeiaufwuf"""
     file = open("pset1.1.py")
     text = file.read()
     file.close()
     pset = open("pset.py", "w")
-    pset.write("s = 'azcbobobegghakl'\n")
+    pset.write("s = 'ilatudolzeiaufwuf'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of vowels: 9").exit(0)
+
+
+@check50.check(exists)
+def oczymsceiif():
+    """correctly identifies the number of vowels in oczymsceiif"""
+    file = open("pset1.1.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'oczymsceiif'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of vowels: 4").exit(0)
+
+@check50.check(exists)
+def odohviuy():
+    """correctly identifies the number of vowels in odohviuy"""
+    file = open("pset1.1.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'odohviuy'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of vowels: 4").exit(0)
+
+@check50.check(exists)
+def kieaoztpfnbyneuovdi():
+    """correctly identifies the number of vowels in kieaoztpfnbyneuovdi"""
+    file = open("pset1.1.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'kieaoztpfnbyneuovdi'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of vowels: 8").exit(0)
+
+@check50.check(exists)
+def mvohwqhooiiaoaxiedxazuw():
+    """correctly identifies the number of vowels in mvohwqhooiiaoaxiedxazuw"""
+    file = open("pset1.1.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'mvohwqhooiiaoaxiedxazuw'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of vowels: 12").exit(0)
+
+@check50.check(exists)
+def swmppnovfoiarkgjbobcce():
+    """correctly identifies the number of vowels in swmppnovfoiarkgjbobcce"""
+    file = open("pset1.1.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'swmppnovfoiarkgjbobcce'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of vowels: 6").exit(0)
+
+@check50.check(exists)
+def kjxhnsaeahk():
+    """correctly identifies the number of vowels in kjxhnsaeahk"""
+    file = open("pset1.1.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'kjxhnsaeahk'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of vowels: 3").exit(0)
+
+@check50.check(exists)
+def dvqzsihabvpd():
+    """correctly identifies the number of vowels in dvqzsihabvpd"""
+    file = open("pset1.1.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'dvqzsihabvpd'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of vowels: 2").exit(0)
+
+@check50.check(exists)
+def gkuaiah():
+    """correctly identifies the number of vowels in gkuaiah"""
+    file = open("pset1.1.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'gkuaiah'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of vowels: 4").exit(0)
+
+@check50.check(exists)
+def bkkloeypovpqkexjgt():
+    """correctly identifies the number of vowels in bkkloeypovpqkexjgt"""
+    file = open("pset1.1.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'bkkloeypovpqkexjgt'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of vowels: 4").exit(0)
+
+@check50.check(exists)
+def yixzwv():
+    """correctly identifies the number of vowels in yixzwv"""
+    file = open("pset1.1.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'yixzwv'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of vowels: 1").exit(0)
+
+@check50.check(exists)
+def cdgoxiforozpyas():
+    """correctly identifies the number of vowels in cdgoxiforozpyas"""
+    file = open("pset1.1.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'cdgoxiforozpyas'\n")
     pset.write(text)
     pset.close()
     check50.run("python3 pset.py").stdout("Number of vowels: 5").exit(0)
 
-
 @check50.check(exists)
-def bzcbcbcbdgghfkl():
-    """correctly identifies the number of vowels in bzcbcbcbdgghfkl"""
+def gkbrloukauoevzhb():
+    """correctly identifies the number of vowels in gkbrloukauoevzhb"""
     file = open("pset1.1.py")
     text = file.read()
     file.close()
     pset = open("pset.py", "w")
-    pset.write("s = 'bzcbcbcbdgghfkl'\n")
+    pset.write("s = 'gkbrloukauoevzhb'\n")
     pset.write(text)
     pset.close()
-    check50.run("python3 pset.py").stdout("Number of vowels: 0").exit(0)
+    check50.run("python3 pset.py").stdout("Number of vowels: 6").exit(0)
+
+@check50.check(exists)
+def tjvaioipehuueclatzuog():
+    """correctly identifies the number of vowels in tjvaioipehuueclatzuog"""
+    file = open("pset1.1.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'tjvaioipehuueclatzuog'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of vowels: 11").exit(0)
+
+@check50.check(exists)
+def wnilguoosegru():
+    """correctly identifies the number of vowels in wnilguoosegru"""
+    file = open("pset1.1.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'wnilguoosegru'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of vowels: 6").exit(0)
+
+@check50.check(exists)
+def deiakeg():
+    """correctly identifies the number of vowels in deiakeg"""
+    file = open("pset1.1.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'deiakeg'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of vowels: 4").exit(0)
+
+@check50.check(exists)
+def hvraalqkiyuwxtgose():
+    """correctly identifies the number of vowels in hvraalqkiyuwxtgose"""
+    file = open("pset1.1.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'hvraalqkiyuwxtgose'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of vowels: 6").exit(0)
+
+@check50.check(exists)
+def sxuovlqudmeqkovqxpvaumm():
+    """correctly identifies the number of vowels in sxuovlqudmeqkovqxpvaumm"""
+    file = open("pset1.1.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'sxuovlqudmeqkovqxpvaumm'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of vowels: 7").exit(0)
+
+@check50.check(exists)
+def toayuldwghitnjcnuv():
+    """correctly identifies the number of vowels in toayuldwghitnjcnuv"""
+    file = open("pset1.1.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'toayuldwghitnjcnuv'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of vowels: 5").exit(0)
+
+@check50.check(exists)
+def uahrjbwbosxpokdl():
+    """correctly identifies the number of vowels in uahrjbwbosxpokdl"""
+    file = open("pset1.1.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'uahrjbwbosxpokdl'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of vowels: 4").exit(0)
