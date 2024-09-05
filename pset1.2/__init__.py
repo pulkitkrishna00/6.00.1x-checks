92fd91abe045c086776dd7c1313cb6e55cce778a
Pulkit Krishna <contact@virtualhub.eu.org>
2024-09-05 13:17:19 +0530

    PSET 1.2

---
+++ pset1.2/__init__.py
@@ -0,0 +1,248 @@
+import check50
+
+
+@check50.check()
+def exists():
+    """pset1.2.py exists"""
+    check50.exists("pset1.2.py")
+
+
+@check50.check(exists)
+def bobboboobobobwobobzoboobbobbobooxobobobobob():
+    """correctly identifies the number of "bob"" in bobboboobobobwobobzoboobbobbobooxobobobobob"""
+    file = open("pset1.2.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'bobboboobobobwobobzoboobbobbobooxobobobobob'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of times bob occurs is: 11").exit(0)
+
+@check50.check(exists)
+def qyobooobobobobbfboobiobobbobbobobobmubboobobobo():
+    """correctly identifies the number of "bob"" in qyobooobobobobbfboobiobobbobbobobobmubboobobobo"""
+    file = open("pset1.2.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'qyobooobobobobbfboobiobobbobbobobobmubboobobobo'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of times bob occurs is: 10").exit(0)
+
+@check50.check(exists)
+def obooobobbbobbbbobobobboqobobboobxbobbhboobobob():
+    """correctly identifies the number of "bob"" in obooobobbbobbbbobobobboqobobboobxbobbhboobobob"""
+    file = open("pset1.2.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'obooobobbbobbbbobobobboqobobboobxbobbhboobobob'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of times bob occurs is: 9").exit(0)
+
+@check50.check(exists)
+def bfobobooooboboobobkobobo():
+    """correctly identifies the number of "bob"" in bfobobooooboboobobkobobo"""
+    file = open("pset1.2.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'bfobobooooboboobobkobobo'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of times bob occurs is: 4").exit(0)
+
+@check50.check(exists)
+def obobabboobboobbobboobobobrbobobmb():
+    """correctly identifies the number of "bob"" in obobabboobboobbobboobobobrbobobmb"""
+    file = open("pset1.2.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'obobabboobboobbobboobobobrbobobmb'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of times bob occurs is: 6").exit(0)
+
+@check50.check(exists)
+def hebobbbobbobbbobbobobbojoboo():
+    """correctly identifies the number of "bob"" in hebobbbobbobbbobbobobbojoboo"""
+    file = open("pset1.2.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'hebobbbobbobbbobbobobbojoboo'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of times bob occurs is: 6").exit(0)
+
+@check50.check(exists)
+def dtboobbboobbobbqobooiubovbobobbobbq():
+    """correctly identifies the number of "bob"" in dtboobbboobbobbqobooiubovbobobbobbq"""
+    file = open("pset1.2.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'dtboobbboobbobbqobooiubovbobobbobbq'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of times bob occurs is: 4").exit(0)
+
+@check50.check(exists)
+def cipojnrlvl():
+    """correctly identifies the number of "bob"" in cipojnrlvl"""
+    file = open("pset1.2.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'cipojnrlvl'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of times bob occurs is: 0").exit(0)
+
+@check50.check(exists)
+def bobobobobobobobobob():
+    """correctly identifies the number of "bob"" in bobobobobobobobobob"""
+    file = open("pset1.2.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'bobobobobobobobobob'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of times bob occurs is: 9").exit(0)
+
+@check50.check(exists)
+def mnffbobbvobobborobobbonbobob():
+    """correctly identifies the number of "bob"" in mnffbobbvobobborobobbonbobob"""
+    file = open("pset1.2.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'mnffbobbvobobborobobbonbobob'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of times bob occurs is: 5").exit(0)
+
+@check50.check(exists)
+def boobbobbobbdkbobeboobl():
+    """correctly identifies the number of "bob"" in boobbobbobbdkbobeboobl"""
+    file = open("pset1.2.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'boobbobbobbdkbobeboobl'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of times bob occurs is: 3").exit(0)
+
+@check50.check(exists)
+def oboboboobbobfboobobobbobobanpboobobonobobobobco():
+    """correctly identifies the number of "bob"" in oboboboobbobfboobobobbobobanpboobobonobobobobco"""
+    file = open("pset1.2.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'oboboboobbobfboobobobbobobanpboobobonobobobobco'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of times bob occurs is: 11").exit(0)
+
+@check50.check(exists)
+def obbobobobobobqobyfbao():
+    """correctly identifies the number of "bob"" in obbobobobobobqobyfbao"""
+    file = open("pset1.2.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'obbobobobobobqobyfbao'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of times bob occurs is: 5").exit(0)
+
+@check50.check(exists)
+def obobboboboboboomobobboobpobswm():
+    """correctly identifies the number of "bob"" in obobboboboboboomobobboobpobswm"""
+    file = open("pset1.2.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'obobboboboboboomobobboobpobswm'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of times bob occurs is: 6").exit(0)
+
+@check50.check(exists)
+def mobobbobpdbooboboqxgboobn():
+    """correctly identifies the number of "bob"" in mobobbobpdbooboboqxgboobn"""
+    file = open("pset1.2.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'mobobbobpdbooboboqxgboobn'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of times bob occurs is: 3").exit(0)
+
+@check50.check(exists)
+def obobdboobboobboobbobbobb():
+    """correctly identifies the number of "bob"" in obobdboobboobboobbobbobb"""
+    file = open("pset1.2.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'obobdboobboobboobbobbobb'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of times bob occurs is: 3").exit(0)
+
+@check50.check(exists)
+def dfbobbgsobobobikobobooboboboobc():
+    """correctly identifies the number of "bob"" in dfbobbgsobobobikobobooboboboobc"""
+    file = open("pset1.2.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'dfbobbgsobobobikobobooboboboobc'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of times bob occurs is: 6").exit(0)
+
+@check50.check(exists)
+def obdvbooboboboboobobobobobnbobob():
+    """correctly identifies the number of "bob"" in obdvbooboboboboobobobobobnbobob"""
+    file = open("pset1.2.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'obdvbooboboboboobobobobobnbobob'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of times bob occurs is: 9").exit(0)
+
+@check50.check(exists)
+def qboboobbobburhgs():
+    """correctly identifies the number of "bob"" in qboboobbobburhgs"""
+    file = open("pset1.2.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'qboboobbobburhgs'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of times bob occurs is: 2").exit(0)
+
+@check50.check(exists)
+def yfiboobobobbbobobkbobboboobob():
+    """correctly identifies the number of "bob"" in yfiboobobobbbobobkbobboboobob"""
+    file = open("pset1.2.py")
+    text = file.read()
+    file.close()
+    pset = open("pset.py", "w")
+    pset.write("s = 'yfiboobobobbbobobkbobboboobob'\n")
+    pset.write(text)
+    pset.close()
+    check50.run("python3 pset.py").stdout("Number of times bob occurs is: 7").exit(0)
